d8583c008a6f7a987dc3e81a63f309407ee4ba4d
Samash92 <tsampson2092@gmail.com>
2019-04-27 22:26:02 -0700

    Added camera movement (wasd and mouse wheel rotation), collectible spawning and room boundaries

---
+++ CoobSim/Assets/Scripts/InputSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSpawn : MonoBehaviour {
+
+    [Header("Object to Spawn")]
+    public GameObject spawnable;
+
+    [Header("Player Object")]
+    public GameObject player;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            Debug.Log("Pressed primary button.");
+            Vector3 temp = Input.mousePosition;
+            temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+            Object.Instantiate(spawnable, Camera.main.ScreenToWorldPoint(temp), transform.rotation);
+         
+        }
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+}
