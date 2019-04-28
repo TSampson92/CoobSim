5a3aecf8d37cae069356f74fd6a8e53561e08f07
Samash92 <tsampson2092@gmail.com>
2019-04-27 22:26:02 -0700

    Added camera movement (wasd and mouse wheel rotation), collectible spawning and room boundaries

---
+++ CoobSim/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    [Header("Camera Speed")]
+    public int Speed = 50;
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed;
+        float zAxisValue = Input.GetAxis("Vertical") * Speed;
+        float rotation = Input.GetAxis("Mouse ScrollWheel") * Speed;
+
+        transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + rotation, transform.rotation.z, transform.rotation.w);
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+    }
+}
