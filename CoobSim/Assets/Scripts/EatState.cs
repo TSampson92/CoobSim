08765e3b03ed631853c3b15034159726695fac97
rtustison <rachaeltustison@gmail.com>
2019-04-28 21:53:16 -0700

    Added a color change when cube gets close to an orb

--- CoobSim/Assets/Scripts/EatState.cs
+++ CoobSim/Assets/Scripts/EatState.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class EatState : StateMachineBehaviour {
 
     public float speed = 1.0f;
-
+    public GameObject FloatingTextPrefab;
     private Walk walk;
+    Renderer rend;
+    bool isClose = false;
+    
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         Debug.Log("We have entered the eat state");
         walk = animator.gameObject.GetComponent<Walk>();
+        rend = animator.gameObject.GetComponent<Renderer>();
+        //rend.material.SetColor("_Color", Color.green);
+
     }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        if(animator.GetFloat("distanceFromWaypoint") < 0.001f)
+        if (animator.GetFloat("distanceFromWaypoint") < 0.5f && !isClose)
+        {
+            rend.material.SetColor("_Color", Color.green);
+            isClose = true;
+        }
+        if (animator.GetFloat("distanceFromWaypoint") < 0.001f)
         {
             var collectibles = GameObject.FindGameObjectsWithTag("Collectible");
             var objectCount = collectibles.Length;
@@ -27,14 +38,19 @@ public class EatState : StateMachineBehaviour {
                 {
                     Debug.Log("Found it");
                     Destroy(item);
+                    isClose = false;
                     break;
                 }
             }
+            rend.material.SetColor("_Color", Color.red);
         }
+        
     }
+
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         Debug.Log("We have left the eat state");
+        rend.material.SetColor("_Color", Color.red);
     }
 }
